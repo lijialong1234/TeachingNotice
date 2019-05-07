94a874c1d4b907a79739d0a7c9d754af4326c0ac
lijialong1234 <2538436020@qq.com>
2019-05-07 15:07:06 +0800

    Add files via upload
    add gsoap 逻辑

---
+++ gsoap/soapAPI.h
@@ -0,0 +1,7 @@
+//gsoap ns service name: soapAPI
+//gsoap ns service style: rpc 
+
+int ns__add(int num1, int num2, int* result );
+int ns__sub(int num1, int num2, int* result );
+int ns__mult( int num1, int num2, int *result);
+int ns__divid( int num1, int num2, int *result);
